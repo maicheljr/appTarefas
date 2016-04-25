f7456be3441789f5a7628f53d2f542dbeaecbc97
maicheljr <maicheljr@gmail.com>
2016-04-25 15:00:17 -0300

    25042016
    inserir campos, cidade, uf, data,  cadastro de clientes
    listar ordenado

---
+++ www/js/UtilModel.js
@@ -0,0 +1,9 @@
+/*angular.module('stater',['ionic','ngCordova'])
+  .factory("UtilModel", function($cordovaToast){
+
+    Util.show = function(message,duration,location){
+      $cordovaToast.show(message,duration,location);
+    }
+
+})
+*/
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ngCordova'])
+angular.module('starter', ['ionic','ngCordova','ui.mask'])
 
 .config(function ($ionicConfigProvider){
   $ionicConfigProvider.tabs.position('bottom');
@@ -43,118 +43,150 @@ angular.module('starter', ['ionic','ngCordova'])
 
 })
 
-.controller("ClienteCtrl", function($scope,$ionicPopup,$ionicListDelegate,$cordovaGeolocation) {
+.controller("ClienteCtrl", function($scope,$ionicPopup,$ionicListDelegate,$cordovaGeolocation,$cordovaToast) {
+
+  $scope.showToast = function(message,duration,location){
+    $cordovaToast.show(message,duration,location);
+  }
 
 
 
   function getNovoCliente(item,novo ){
 
-
-
     $scope.data = {};
     $scope.data.nome=item.nome;
     $scope.data.telefone=item.telefone;
     $scope.data.endereco=item.endereco;
+    $scope.data.uf=item.uf;
+    $scope.data.cidade=item.cidade;
+    $scope.data.date=item.date;
     $scope.data.lat = item.lat;
     $scope.data.lng = item.lng;
 
     /**/
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-       alert( 'Geolocation is not supported by this browser');
+      alert( 'Não suporta GPS via Browser!!');
     }
     var latitude = '';
     var longitude = '';
 
-function showPosition(position) {
 
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
 
-    $scope.latitude = latitude.toFixed(5);
-    $scope.longitude = longitude.toFixed(5);
+    function showPosition(position) {
 
-    $ionicPopup.show({
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
 
-      title:"<b>Novo Cliente<b>",
-      scope:$scope,
-      template:
-      "<div class='list list-inset'>"+
-      " <label class='item item-input'> <input type='text' placeholder='Nome' autofocus='true' ng-model='data.nome'>  </label>"+
-      " <label class='item item-input'> <input type='tel' placeholder='Telefone'ng-model='data.telefone'>  </label> "+
-      " <label class='item item-input'> <input type='text' placeholder='Endereco'ng-model='data.endereco'>  </label>"+
-      " <label class='item item-input'> <input type='text'   ng-model='data.lat' >"+$scope.latitude+"</input></label>"+
-      " <label class='item item-input'> <input type='text'   ng-model='data.lng'>"+$scope.longitude+"</input></label>"+
-      " </div>",
-      buttons:[
-        {text:"<b>Salvar</b>",
-        onTap: function (e){
+      $scope.latitude = latitude.toFixed(5);
+      $scope.longitude = longitude.toFixed(5);
 
-          if($scope.data.nome  && $scope.data.telefone  && $scope.data.endereco){
-            item.nome = $scope.data.nome;
-            item.telefone = $scope.data.telefone;
-            item.endereco = $scope.data.endereco;
-            item.lat = $scope.latitude;
-            item.lng =  $scope.longitude;
+      $ionicPopup.show({
 
-          if(novo){
-              cliente.add(item);
-            }
+        title:"<b>Novo Cliente<b>",
+        scope:$scope,
+        template:
+        "<div class='list list-inset'>"+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>Nome</span> <input type='text'placeholder='Nome completo' autofocus='true' ng-model='data.nome'>  </label>"+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>Telefone</span><input type='tel' ui-mask='(99) 9999-9999?9' ng-model='data.telefone'>  </label> "+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>Endereço</span><input type='text' placeholder='Av, Rua, nº'ng-model='data.endereco'>  </label>"+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>UF</span><input type='text' placeholder='RS'ng-model='data.uf'>  </label>"+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>Cidade</span><input type='text' placeholder='Santa Marias'ng-model='data.cidade'>  </label>"+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>Data</span><input  data-date-format='DD MMMM YYYY'type='date' ng-model='data.date'>  </label> "+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>Latitude</span><input type='text' placeholder="+$scope.latitude+" ng-model='data.lat' ></input></label>"+
+        " <label class='item item-input item-stacked-label'><span class='input-label'>Longitude</span><input type='text' placeholder="+$scope.longitude+" ng-model='data.lng'></input></label>"+
+        " </div>",
+        buttons:[
+          {text:"<b>Salvar</b>",
+          onTap: function (e){
+
+            if($scope.data.nome  && $scope.data.telefone  && $scope.data.endereco  && $scope.data.uf && $scope.data.cidade){
+              item.nome = $scope.data.nome;
+              item.telefone = $scope.data.telefone;
+              item.endereco = $scope.data.endereco;
+              item.date = $scope.data.date;
+              item.uf = $scope.data.uf;
+              item.cidade = $scope.data.cidade;
+              item.lat = $scope.latitude;
+              item.lng =  $scope.longitude;
+
+              if(novo){
+                cliente.add(item);
+                //$scope.showToast('Cliente cadastrado com sucesso!!!','long','center');
+              }
               cliente.save();
+              //$scope.showToast('Cliente editado com sucesso!!!','short','center');
             }else{
-              alert("Há Campos vazios!!!")}
-              //onClienteAdd();
-            },
+              alert("Há Campos vazios!!!");
+              //$scope.showToast('Preencha todos os campos!!!','long','center');
+            }
+            //onClienteAdd();
+          },
 
-              type:"button-assertive"},
+          type:"button-assertive"},
 
-            {text:"<b>Sair</b>"}
+          {text:"<b>Sair</b>"}
 
         ]
       });
 
-
-
-}
-
-
-
-      $ionicListDelegate.closeOptionButtons();
-    };
-
-    //criando o objeto cliente
-    var cliente = new getCliente();
-    //var pos = new getCoordenada();
-
-
-    // criando uma lista q irá receber os clientes
-    $scope.lista =  cliente.items;
-
-
-    $scope.onClienteRemove = function(item){
-      cliente.remove(item);
-      cliente.save();
-      $ionicListDelegate.closeOptionButtons();
-    };
-
-    //metodo que adiciona novos clientes
-    $scope.onClienteAdd = function(){
-      var item = {nome:"",telefone:"",endereco:"",lat:"",lng:""};
-      getNovoCliente(item,true);
-      //alert("Lat: "+lat+"Lng: "+long);
-
-    };
-
-    $scope.onClienteEdit = function (item){
-      getNovoCliente(item, false);
-      cliente.save();
     }
 
-  })
-  .controller('MapCtrl', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
+    $ionicListDelegate.closeOptionButtons();
+  };
 
-    $scope.onMap = function() {
+  //criando o objeto cliente
+  var cliente = new getCliente();
+  //var pos = new getCoordenada();
+
+
+  // criando uma lista q irá receber os clientes
+  $scope.lista =  cliente.items;
+
+
+  $scope.onClienteRemove = function(item){
+    cliente.remove(item);
+    cliente.save();
+    $ionicListDelegate.closeOptionButtons();
+  };
+
+  //metodo que adiciona novos clientes
+  $scope.onClienteAdd = function(){
+    var item = {nome:"",telefone:"",endereco:"",uf:"",cidade:"",date:"",lat:"",lng:""};
+    getNovoCliente(item,true);
+    //alert("Lat: "+lat+"Lng: "+long);
+
+  };
+
+  $scope.onClienteEdit = function (item){
+    getNovoCliente(item, false);
+    cliente.save();
+  }
+
+  $scope.Upper = function(name){
+    var nomeOut = name.toUpperCase(name);
+    return nomeOut;
+  };
+
+
+})
+.controller('MapCtrl', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform,$cordovaToast) {
+
+  var cliente = new getCliente();
+  $scope.listaCoordenadas =  cliente.items;
+
+  $scope.showToast = function(message, duration, location) {
+    $cordovaToast.show(message, duration, location).then(function(success) {
+
+    }, function (error) {
+
+    });
+  }
+
+  $scope.onMap = function(menu) {
+
+    if (menu == 1){
 
       $ionicLoading.show({
         template: '<ion-spinner class="spinner-assertive" icon="android"></ion-spinner><br/>Procurando Localização!'
@@ -166,19 +198,15 @@ function showPosition(position) {
         maximumAge: 0
       };
       $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+
         var lat  = position.coords.latitude;
         var long = position.coords.longitude;
-
         var myLatlng = new google.maps.LatLng(lat, long);
-
-
         var mapOptions = {
           center: myLatlng,
           zoom: 15,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
-
 
         var map = new google.maps.Map(document.getElementById("map"), mapOptions),
         marker = new google.maps.Marker({
@@ -197,84 +225,134 @@ function showPosition(position) {
         $ionicLoading.hide();
         //console.log(err);
       });
-
-    }
-  })
-
-  .controller('AgendaCtrl', function($scope,$ionicPopup,$ionicListDelegate,$ionicLoading){
-
-    var cliente = new getCliente();
-    var agenda = new getAgenda();
-
-    $scope.listarCliente = cliente.items;
-    $scope.listarAgenda = agenda.items;
-    $scope.deleteAgenda = false;
-
-    $scope.formatDate = function(date){
-             var dateOut = new Date(date);
-             return dateOut;
-       };
-
-    $scope.onClickRemoveAgenda = function(){
-      $scope.deleteAgenda = !$scope.deleteAgenda;
     }
 
-    $scope.onAgendaRemove = function(item){
-      agenda.remove(item);
-      agenda.save();
-      $ionicListDelegate.closeOptionButtons();
-    };
+    // listando todos os clientes no mapa
+    if (menu == 2){
 
-    //metodo que adiciona novos clientes
-    $scope.onAgendaAdd = function(){
-      var item = {nome:"",date:"",hora:""};
-      getNovaAgenda(item,true);
-      //agenda.save();
-
-    };
-
-    $scope.onAgendaEdit = function (item){
-      getNovaAgenda(item, false);
-      agenda.save();
-    };
-
-    function getNovaAgenda(item,novo ){
-      $scope.data = {};
-      $scope.data.nome=item.nome;
-      $scope.data.date=item.date;
-      $scope.data.hora=item.hora;
-
-      $ionicPopup.show({
-        title:"<b>Novo Agendamento<b>",
-        scope:$scope,
-        template:"<div class='list list-inset'>"+
-        "<label class='item item-input'><select ng-model='data.cliente' ng-options='item.nome for item in listarCliente track by item.nome'>"+
-        "<option value=''>{{item.nome}}</option></select></label>"+
-        " <label class='item item-input'> <input type='text' placeholder='{{data.cliente.nome}}' ></label>"+
-        " <label class='item item-input'> <input  data-date-format='DD MMMM YYYY'type='date' ng-model='data.date'>  </label> "+
-        " <label class='item item-input'> <input type='text' placeholder='Hora'ng-model='data.hora'>  </label> "+
-        " </div>",
-        buttons:[
-          {text:"<b>Salvar</b>",
-          onTap: function (e){
-            if ($scope.data.cliente && $scope.data.date && $scope.data.hora) {
-              item.nome = $scope.data.cliente.nome;
-              item.date = $scope.data.date;
-              item.hora = $scope.data.hora;
-              if(novo){
-                agenda.add(item);
-              }
-              agenda.save();
-            }else{alert("Há Campos vazios!!!")}
-          },
-          type:'button-assertive'},
-          {text:"<b>Cancelar</b>"}
-        ]
+      $ionicLoading.show({
+        template: '<ion-spinner class="spinner-assertive" icon="android"></ion-spinner><br/>Procurando Localização!'
       });
-      $ionicListDelegate.closeOptionButtons();
-    };
+
+      var posOptions = {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0
+      };
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+
+        var lat  = position.coords.latitude;
+        var long = position.coords.longitude;
+        var myLatlng = new google.maps.LatLng(lat, long);
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        //alert("->" + $scope.lista.);
+
+        for(i = 0;i < $scope.listaCoordenadas.length;i++){
+
+          //alert($scope.listaCoordenadas[i].lat+","+$scope.listaCoordenadas[i].lng);
+          var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng($scope.listaCoordenadas[i].lat,$scope.listaCoordenadas[i].lng),
+            map: map,
+            icon: 'img/marcador_32.png',
+            draggable:true
+          });
+
+          $ionicLoading.hide();
+          $scope.map = map;
+
+        }// fim do for
+        $ionicLoading.hide();
+
+      }, function(err) {
+        $ionicLoading.hide();
+        //console.log(err);
+      });
+    }// fecha o menu 02
+  } //fecha onMap;
+})//fecha  controlLer
+
+.controller('AgendaCtrl', function($scope,$ionicPopup,$ionicListDelegate,$ionicLoading){
+
+  var cliente = new getCliente();
+  var agenda = new getAgenda();
+
+  $scope.listarCliente = cliente.items;
+  $scope.listarAgenda = agenda.items;
+  $scope.deleteAgenda = false;
 
 
 
 
-  })
+  $scope.formatDate = function(date){
+    var dateOut = new Date(date);
+    return dateOut;
+  };
+
+  $scope.onClickRemoveAgenda = function(){
+    $scope.deleteAgenda = !$scope.deleteAgenda;
+  }
+
+  $scope.onAgendaRemove = function(item){
+    agenda.remove(item);
+    agenda.save();
+    $ionicListDelegate.closeOptionButtons();
+  };
+
+  //metodo que adiciona novos clientes
+  $scope.onAgendaAdd = function(){
+    var item = {nome:"",date:"",hora:""};
+    getNovaAgenda(item,true);
+    //agenda.save();
+
+  };
+
+  $scope.onAgendaEdit = function (item){
+    getNovaAgenda(item, false);
+    agenda.save();
+  };
+
+  function getNovaAgenda(item,novo ){
+    $scope.data = {};
+    $scope.data.nome=item.nome;
+    $scope.data.date=item.date;
+    $scope.data.hora=item.hora;
+
+    $ionicPopup.show({
+      title:"<b>Novo Agendamento<b>",
+      scope:$scope,
+      template:"<div class='list list-inset'>"+
+      "<label class='item item-input'><select ng-model='data.cliente' ng-options='item.nome for item in listarCliente track by  item.nome'>"+
+      "<option value=''>{{item.nome}}</option></select></label>"+
+      " <label class='item item-input'> <input type='text' placeholder='{{data.cliente.nome}}' ></label>"+
+      " <label class='item item-input'> <input  data-date-format='DD MMMM YYYY'type='date' ng-model='data.date'>  </label> "+
+      " <label class='item item-input'> <input type='text' ui-mask='99:9?9' ng-model='data.hora'>  </label> "+
+      " </div>",
+      buttons:[
+        {text:"<b>Salvar</b>",
+        onTap: function (e){
+          if ($scope.data.cliente && $scope.data.date && $scope.data.hora) {
+            item.nome = $scope.data.cliente.nome;
+            item.date = $scope.data.date;
+            item.hora = $scope.data.hora;
+            if(novo){
+              agenda.add(item);
+            }
+            agenda.save();
+          }else{alert("Há Campos vazios!!!")}
+        },
+        type:'button-assertive'},
+        {text:"<b>Cancelar</b>"}
+      ]
+    });
+    $ionicListDelegate.closeOptionButtons();
+  };
+
+
+
+
+})
